26783095e7bfac5bd4e4261065bc355806d6cbcc
Elvis Hernandez <you@example.com>
2019-04-14 21:22:28 -0500

    Pending Dashboard now

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -43,80 +43,89 @@ export default class Dashboard extends Component {
             <div className='pendingModalCon'>
             <div className='pendingModal'>
                 <div className='CompanyName'>
-                  {/* <h5>Company Name</h5> */}
-                  <p><b>WeWork LLC</b></p>
+                  <div className="ChoiceTitle"><h5>Company Name</h5></div>
+                  <div className="ChoiceOneName"><p><b>WeWork LLC</b></p></div>
                     <br/>
-                     <p>4/11/2019</p>
+                    <div className="ChoiceTwoName"><p><b>serendipty Labs</b></p></div>
                     <br/>
-                    <p>info@wework.com</p>
+                    <div className="ChoiceThreeName"><p><b>Industrious LLC</b></p></div>
                     <br/>
-                    <p>Pending</p>
-                  <button>Resend</button>
-                  
                 </div>
 
                 <div className='DateSent'>
-                  {/* <h5>Date Sent</h5> */}
-                 <p><b>Serendipty Labs</b></p>
+                <div className="ChoiceDate"><h5>Date Sent</h5></div>
+                <div className="ChoiceOneDate"><p>4/11/2019</p></div>
                     <br/>
-                  <p>4/08/2019</p>
+                    <div className="ChoiceTwoDate"><p>4/08/2019</p></div>
                     <br/>
-                    <p>info@serendiptylabs.com</p>
+                    <div className="ChoiceThreeDate"><p>4/01/2019</p></div>
                     <br/>
-                    <p>Pending</p>
-                  <button>Resend</button>
-                    {/* <br/> */}
                 </div>
 
                 <div className='Email'>
-                  {/* <h5>Email</h5> */}
-                  <p><b>Industrious LLC</b></p>
+                <div className="ChoiceEmail"><h5>Email</h5></div>
+                <div className="ChoiceOneEmail"><p>info@wework.com</p></div>
                     <br/>
-                  <p>info@Industriousoffice.com</p>
+                    <div className="ChoiceTwoEmail"><p>info@serendiptylabs.com</p></div>
                     <br/>
-                  <p>4/01/2019</p>
+                    <div className="ChoiceThreeEmail"><p>info@industrious.com</p></div>
                     <br/>
-                  <p>Expired</p>
-                  <button>Resend</button>
-                  
                 </div>
 
                 <div className='Status'>
-                  {/* <h5>Status</h5> */}
-                  
-                    {/* <br/> */}
-                  
-                </div>                  
+                <div className="ChoiceStatus"><h5>Status</h5></div>
+                <div className="ChoiceOneStatus"><p>Pending</p></div>
+                  <br/>
+                  <div className="ChoiceTwoStatus"><p>Pending</p></div>
+                  <br/>
+                  <div className="ChoiceThreeStatus"><p>Pending</p></div>
+                  <br/>
+                  </div> 
+
+                <div className='Resend'>
+                <div className="ChoiceResend"><h5>Resend</h5></div>
+                <div className="ChoiceOneResend">
+                  <button>Resend</button></div>
+                  <br/>
+                  <div className="ChoiceTwoResend">
+                  <button>Resend</button></div>
+                  <br/>
+                  <div className="ChoiceThreeResend">
+                  <button>Resend</button></div>
+                  <br/>
                 </div>                  
                                 
 
             {console.log('Acutally Jack', this.state.pending)}
               </div>
+              </div>
             )
 
 
         const RecievedCompanies = () => (
-            <div className='recievedModalCon'>
             <div className='recievedModal'>
                 <div className='CompanyName'>
                   {/* <h5>Company Name</h5> */}
-                  <p><b>Industrious LLC</b></p>
+                  <div className="theone"><p><b>Industrious LLC</b></p></div>
                   <br/>
-                  <p>4/04/2019</p>
-                  <p>info@Industriousoffice.com</p>
+                  <div><p><b>wework</b></p></div>
+                  <br/>
+                  <div><p><b>serendiptylabs</b></p></div>
+                  {/* <p>4/04/2019</p> */}
+                  {/* <p>info@Industriousoffice.com</p> */}
                     <br/>
-                    <p>Pending</p>
+                    {/* <p>Pending</p> */}
                     {/* <button>Resend</button> */}
                     {/* <br/> */}
                 </div>
                 <div className='DateRecieved'>
-                    <p><b>WeLive LLC</b></p>
+                    {/* <p><b>WeLive LLC</b></p> */}
                     <br/>
                     <p>4/02/2019</p>
                     <br/>
-                    <p>info@welive.com</p>
+                    {/* <p>info@welive.com</p> */}
                     <br/>
-                    <p>Pending</p>
+                    {/* <p>Pending</p> */}
                   {/* <h5>Date Recieved</h5> */}
                 <div className='Email'>
                   <p><b>Serendipty Labs</b></p>
@@ -145,7 +154,6 @@ export default class Dashboard extends Component {
                   
                   {/* <button>Resend</button> */}
                 </div>   
-                </div>   
 
 
 
@@ -159,16 +167,12 @@ export default class Dashboard extends Component {
                 <div className='Dashboard'>
                 <h1 className='DashboardTitle'>Client Dashboard</h1>
                 <div className="cLog"><button className="logout"><a href='http://localhost:3000/'>Logout</a></button></div>
-                {/* <div className="Buttons"> */}
                 <div className="pebutton"><button className='pending' onClick={this.handlePending}>Pending</button></div>
                 <div className="vebutton"><Link to="/vendorwelcome"><button className='ven'>Add a vendor</button></Link></div>
                 <div className="rebutton"><button className='recieved' onClick={this.handleRecieved}>Recieved</button></div>
-                {/* </div> */}
 
-                <div className="DropOpCon"><div className="DropOp">
                 {this.state.pending && <PendingCompanies />}
                 {!this.state.pending && <RecievedCompanies />}
-                </div></div>
                 
                 </div> 
                 
