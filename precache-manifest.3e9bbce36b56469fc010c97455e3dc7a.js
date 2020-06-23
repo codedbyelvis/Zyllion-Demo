e9d23ad013c8a325e3f1777747d2fc9cb7a7a514
Belvis <elvishernandez5@yahoo.com>
2020-06-23 15:46:04 -0500

    Updates

---
+++ precache-manifest.3e9bbce36b56469fc010c97455e3dc7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "51ba80b70b11b7d6243d",
+    "url": "/Zyllion-Demo/static/css/main.029a8990.chunk.css"
+  },
+  {
+    "revision": "51ba80b70b11b7d6243d",
+    "url": "/Zyllion-Demo/static/js/main.67852c68.chunk.js"
+  },
+  {
+    "revision": "ca0b35f66d65f6a5ebbc",
+    "url": "/Zyllion-Demo/static/js/runtime~main.d90c46d2.js"
+  },
+  {
+    "revision": "eb2ffd550759ca3e3deb",
+    "url": "/Zyllion-Demo/static/js/2.7a7c2527.chunk.js"
+  },
+  {
+    "revision": "6079af6454aa2df0519d2455f0f738f0",
+    "url": "/Zyllion-Demo/static/media/Zyllion Logo.6079af64.png"
+  },
+  {
+    "revision": "ef327a8b63d21455e703b3e6c56da894",
+    "url": "/Zyllion-Demo/index.html"
+  }
+];
